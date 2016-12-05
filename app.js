7fc5df5a339e168a1249cb1d860ba6c453f09acc
dga <dga@infogones.com>
2016-12-05 17:17:39 +0100

    debug de l'import des fichier js/css/etc...

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var http    = require('http');      // load http module
 var logger  = require('morgan');    // http logger
 var app     = express();            // start express
 var server  = http.createServer(app);   // create http server to be used with soket.io
-var io = require('socket.io').listen(server); // load socket.io and listen to server
+var io      = require('socket.io').listen(server); // load socket.io and listen to server
+var path    = require('path');
 
 // middlewares
-app.use(logger('combined'));
-
+app.use(logger('combined'))
+    .use('/', express.static(path.join(__dirname, '/public')));
+    
 // routes
 app.get('/', function (req, res) {
     res.render('index.jade');
